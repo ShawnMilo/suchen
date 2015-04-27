ed644c2183f2454bbcb82c1d6cc1ec9ea2e98470
Shawn Milochik <shawn@milochik.com>
2015-04-27 17:27:11 -0400

    ARM builds

--- build_multiarch.sh
+++ build_multiarch.sh
@@ -9,3 +9,6 @@ GOARCH=amd64 GOOS=darwin go build -o suchen_OSX_64-bit
 
 GOARCH=amd64 GOOS=windows go build -o suchen_Win64
 GOARCH=386 GOOS=windows go build -o suchen_Win32
+
+GOARCH=arm GOOS=linux GOARM=5 -o suchen_arm5
+GOARCH=arm GOOS=linux GOARM=7 -o suchen_arm7
